b06f9728a9100c563671759e9b1b51dc51e74ef3
Morteza Omar Mohammady <mortezaomarmohammady@gmail.com>
2022-11-05 04:45:34 +0430

    change null to []

--- src/models/usersModel.js
+++ src/models/usersModel.js
@@ -1,56 +1,54 @@
 import mongoose, { model } from "mongoose";
 
-
 const userSchema = new mongoose.Schema({
-    uid: {
-        type: String,
-        unique:true  
-    },
-    name :{
-        type: String,
-        required : true
-    },
-    lastName :{
-        type: String,
-        required : true
-    },
-    email:{
-        type:String,
-        required : true
-    },
-    phone:{
-        type:Number,
-        required: true,
-        unique:true
-    },
-    province :{
-        type: mongoose.Schema.ObjectId,
-        required : true,
-        ref: "provinces",
-    },
-    district :{
-        type: mongoose.Schema.ObjectId,
-        required : true,
-        ref: "districts",
-    },
-    polls: {
-        type: [
-            {
-                pollId: {
-                    type: mongoose.Schema.ObjectId,
-                    ref: "polls",
-                },
-                opIndex: {
-                    type: Number
-                }
-            }
-        ],
-        default :null
-    },
-    profileImag: {
-        type: String
-    }
-
+  uid: {
+    type: String,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  province: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+    ref: "provinces",
+  },
+  district: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+    ref: "districts",
+  },
+  polls: {
+    type: [
+      {
+        pollId: {
+          type: mongoose.Schema.ObjectId,
+          ref: "polls",
+        },
+        opIndex: {
+          type: Number,
+        },
+      },
+    ],
+    default: [],
+  },
+  profileImag: {
+    type: String,
+  },
 });
 
-export default model('users',userSchema);
+export default model("users", userSchema);
